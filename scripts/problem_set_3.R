a0d8d822bc9a2b529d38ee88e9d75ad9b05700ff
sbeltro <sbeltro@gmail.com>
2022-07-26 20:59:57 -0500

    Script | Carga Inicial

---
+++ scripts/problem_set_3.R
@@ -0,0 +1,5 @@
+################################################################################
+# Problem Set 3: Making Money with ML?
+# Autores: Natalia Capacho, Yurani Gonzalez, Sebastian Beltran
+# Big Data - MECA 
+################################################################################
